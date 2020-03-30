1a903a134f5a31b473becbc1cada73fa98d10253
Laura Miller <lmiller.spence@gmail.com>
2020-03-30 13:15:33 +0100

    Added form component which includes the search bar

---
+++ src/components/Form.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+//stateless component
+const Form = () => (
+    <form>
+        <input type="text" />
+        <button>Search</button>
+    </form>
+);
+
+export default Form
