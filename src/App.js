082a7fe106450e5a909bae8c3ef13cf2e5c1f7fa
Laura Miller <lmiller.spence@gmail.com>
2020-03-30 16:18:01 +0100

    Stored the API key as a variable, then inserted it into the fetch url

--- src/App.js
+++ src/App.js
@@ -3,18 +3,17 @@ import './App.css';
 import Form from './components/Form';
 import { render } from '@testing-library/react';
 
-class App extends Component {
-
-
-  // spoonacular api key: 234e1bdf12894e93904c85f10f3ab6e0
+const API_KEY = "234e1bdf12894e93904c85f10f3ab6e0";
   // https://api.spoonacular.com/recipes/search?apiKey=234e1bdf12894e93904c85f10f3ab6e0&q=chicken returns meals with chicken
 
+class App extends Component {
+
   // This will call the API
-  getRecipe = (e) => {
+  getRecipe = async (e) => {
     // recipeName takes what the user submits in the form in Form
     const recipeName = e.target.elements.recipeName.value
     e.preventDefault(); 
-    console.log(recipeName)
+    const api_call = await fetch(`https://api.spoonacular.com/recipes/search?apiKey=${API_KEY}&q=chicken`);
   };
 
  render() { 
